05728e8985956cd74e2791595ac053d7b149f5f6
Andrew Lechlak <AndrewLechlak@gmail.com>
2024-11-08 11:30:46 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // This enables static export mode
+  distDir: 'out', // Specifies 'out' as the output directory for static files
   experimental: {
     esmExternals: 'loose'
   },
   webpack: (config) => {
-    config.externals = [...config.externals, { canvas: "canvas" }]; // required to make pdfjs work
+    config.externals = [...config.externals, { canvas: "canvas" }]; // Required to make pdfjs work
     return config;
   },
 };
